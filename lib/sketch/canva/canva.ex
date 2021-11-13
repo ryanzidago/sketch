76b38b163ce75b6c29cdb7dfb93079e6bf3b49fd
Ryan Zidago <ryanzidago@gmail.com>
2021-11-13 09:16:37 +0100

    added basic tests for draw_rectangle/4 function

--- lib/sketch/canva/canva.ex
+++ lib/sketch/canva/canva.ex
@@ -1,5 +1,50 @@
 defmodule Sketch.Canva do
+  defguard is_within_canva(x, y) when x in 0..(24 - 1) and y in 0..(24 - 1)
+
   def new do
     for x <- 0..(24 - 1), y <- 0..(24 - 1), into: %{}, do: {{x, y}, " "}
+  end
+
+  def draw_rectangle(canva, {x, y}, {w, h}, opts)
+      when is_within_canva(x, y)
+      when is_within_canva(w, h) do
+    fill_character = Keyword.get(opts, :fill_character)
+    outline_character = Keyword.get(opts, :outline_character)
+
+    case {fill_character, outline_character} do
+      {nil, nil} ->
+        {:error, "No fill or outline character selected"}
+
+      {fill_character, nil} ->
+        draw_rectangle_with_single_character(canva, {x, y}, {w, h}, character: fill_character)
+    end
+  end
+
+  defp draw_rectangle_with_single_character(canva, {x, y}, {w, h}, character: character) do
+    cells_to_be_filled = for y <- y..(h - 1 + y), x <- x..(w - 1 + x), do: {y, x}
+    draw_on_canva(canva, cells_to_be_filled, character: character)
+  end
+
+  defp draw_on_canva(canva, _cells_to_be_filled, opts)
+
+  defp draw_on_canva(canva, [], _opts), do: canva
+
+  defp draw_on_canva(canva, [{x, y} | cells_to_be_filled], character: character) do
+    canva = Map.put(canva, {x, y}, character)
+    draw_on_canva(canva, cells_to_be_filled, character: character)
+  end
+
+  def pretty_print(canva) do
+    pretty_canva =
+      for x <- 0..(25 - 1), into: "" do
+        row =
+          for y <- 0..(25 - 1), into: "" do
+            "#{canva[{x, y}]}"
+          end
+
+        row <> "\n"
+      end
+
+    IO.puts(pretty_canva)
   end
 end
--- lib/sketch/canva/canva_test.exs
+++ lib/sketch/canva/canva_test.exs
@@ -3,13 +3,70 @@ defmodule Sketch.CanvaTest do
 
   alias Sketch.Canva
 
+  setup _ do
+    {
+      :ok,
+      opts: [fill_character: "X"]
+    }
+  end
+
   describe "new/0" do
     test "creates a new empty 24 * 24 canva" do
       canva = Canva.new()
 
-      for x <- 0..(24 - 1), y <- 0..(24 - 1) do
+      Enum.each(all_cells(), fn {x, y} ->
         assert Map.get(canva, {x, y}) == " "
-      end
+      end)
     end
+  end
+
+  describe "draw_recantgle/3" do
+    test "draws a rectangle on the canva, at the position {x, y}, whose size is w * h", %{
+      opts: opts
+    } do
+      x = 0
+      y = 0
+      w = 2
+      h = 2
+
+      canva = Canva.draw_rectangle(Canva.new(), {x, y}, {w, h}, opts)
+      drawn_cells = all_cells_for_xy_and_wh({x, y}, {w, h})
+
+      Enum.each(all_cells(), fn {x, y} ->
+        if {x, y} in drawn_cells do
+          assert canva[{x, y}] == opts[:fill_character]
+        else
+          assert canva[{x, y}] == " "
+        end
+      end)
+    end
+
+    test "draws a single cell", %{opts: opts} do
+      x = 0
+      y = 0
+      w = 1
+      h = 1
+
+      canva = Canva.draw_rectangle(Canva.new(), {x, y}, {w, h}, opts)
+      drawn_cells = all_cells_for_xy_and_wh({x, y}, {w, h})
+
+      Enum.each(all_cells(), fn {x, y} ->
+        if {x, y} in drawn_cells do
+          assert canva[{x, y}] == opts[:fill_character]
+        else
+          assert canva[{x, y}] == " "
+        end
+      end)
+    end
+  end
+
+  defp all_cells do
+    for x <- 0..(24 - 1), y <- 0..(24 - 1), into: MapSet.new() do
+      {x, y}
+    end
+  end
+
+  defp all_cells_for_xy_and_wh({x, y}, {w, h}) do
+    for x <- x..(w - 1), y <- y..(h - 1), into: MapSet.new(), do: {x, y}
   end
 end
